9c42089fc3c938b58a234a5a4c6ee41cdd50e314
Ahmet Arda İzci <210504031@ogrenci.harran.edu.tr>
2024-05-27 09:48:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/moviesapp/common/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.moviesapp.common
+
+object Constants {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+
+    const val API_KEY = "802b2c4b88ea1183e50e6b285a27696e"
+
+    const val BASE_IMAGE = "https://image.tmdb.org/t/p/w500"
+}
